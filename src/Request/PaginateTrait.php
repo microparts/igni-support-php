19ed8663466cb65b02531eb42fd6b991b8da45b1
roquie <roquie0@gmail.com>
2018-11-01 20:58:15 +0300

    Trait for pagination.

---
+++ src/Request/PaginateTrait.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by Roquie.
+ * E-mail: roquie0@gmail.com
+ * GitHub: Roquie
+ * Date: 26/10/2018
+ */
+
+namespace Microparts\Igni\Support\Request;
+
+use Microparts\PaginateFormatter\PaginateFormatter;
+use Pagerfanta\Pagerfanta;
+use Psr\Http\Message\ServerRequestInterface;
+
+trait PaginateTrait
+{
+    /**
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param $adapter
+     * @return array
+     */
+    public function paginate(ServerRequestInterface $request, $adapter)
+    {
+        $query = $request->getQueryParams();
+
+        $page = new Pagerfanta($adapter);
+        $page->setCurrentPage((int) ($query['page'] ?? 1));
+        $page->setMaxPerPage((int) ($query['per_page'] ?? 15));
+        $paginate = new PaginateFormatter($page);
+
+        return $paginate->format();
+    }
+}
