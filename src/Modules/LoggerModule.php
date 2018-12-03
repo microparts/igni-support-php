1104d37b5d3591edad593bd975ce8ad3c8c8f6bf
roquie <roquie0@gmail.com>
2018-12-03 04:44:05 +0300

    Logs must be without fucking brackets.

--- src/Modules/LoggerModule.php
+++ src/Modules/LoggerModule.php
@@ -9,6 +9,7 @@
 namespace Microparts\Igni\Support\Modules;
 
 use Igni\Application\Providers\ServiceProvider;
+use Monolog\Formatter\LineFormatter;
 use Monolog\Handler\ErrorLogHandler;
 use Monolog\Logger;
 use Psr\Container\ContainerInterface;
@@ -16,17 +17,23 @@ use Psr\Log\LoggerInterface;
 
 class LoggerModule implements ServiceProvider
 {
+    private const CHANNEL = 'App';
+
     /**
      * @param \Illuminate\Container\Container|ContainerInterface $container
      */
     public function provideServices($container): void
     {
+        // Create Monolog logger without fucking brackets -> [] []  [] []  [] []  [] []  [] []
+        // if context and extra is empty.
         $container->bind(LoggerInterface::class, function () {
-            $logger = new Logger('App');
-            $logger->pushHandler(new ErrorLogHandler(
-                ErrorLogHandler::OPERATING_SYSTEM,
-                $this->chooseLogLevel()
-            ));
+            $logger = new Logger(self::CHANNEL);
+            $handler = new ErrorLogHandler(ErrorLogHandler::OPERATING_SYSTEM, $this->chooseLogLevel());
+            $formatter = new LineFormatter('[%datetime%] %channel%.%level_name%: %message% %context% %extra%');
+            $formatter->ignoreEmptyContextAndExtra();
+
+            $handler->setFormatter($formatter);
+            $logger->pushHandler($handler);
 
             return $logger;
         });
