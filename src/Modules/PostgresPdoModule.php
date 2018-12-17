c67ed10bb8db3c184c8bb4db867a29532bc8c4ef
roquie <roquie0@gmail.com>
2018-12-17 17:40:53 +0300

    Persistent pdo connection.

--- src/Modules/PostgresPdoModule.php
+++ src/Modules/PostgresPdoModule.php
@@ -44,6 +44,7 @@ class PostgresPdoModule implements ServiceProvider
             $container->bind(PDO::class, function () use ($pdo, $conf) {
                 $conn = $this->reconnectIfClosed($pdo, $conf);
                 $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+                $conn->setAttribute(PDO::ATTR_PERSISTENT, true);
 
                 return $conn;
             });
